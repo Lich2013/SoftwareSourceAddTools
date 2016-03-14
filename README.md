3904794a97323d6a7a8c0e05b1d19902638e824b
Lich2013 <longzy2013@gmail.com>
2016-03-14 23:13:50 +0800

    update redis source

---
+++ README.md
@@ -0,0 +1,23 @@
+#Linux作为web服务器环境配置一键安装脚本
+
+- 每天搞点...
+- 默认用阿里云国内使用, 需要换的话就自己改下脚本里的`source`
+- node版本默认为4, 可修改为5
+
+
+## Ubuntu
+
+### stable
+
+- nginx/stable
+
+### dev
+
+- nginx/development
+
+## CentOS
+
+### stable
+
+### dev
+
---
+++ Ubuntu_dev.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+source='mirrors.aliyun.com' #镜像源
+mysql='mysql-apt-config_0.6.0-1_all.deb' #mysql源
+node=4 #选用4或5
+sudo sed -ig  "s@//[^/]*@//$source@" /etc/apt/source.list
+sudo add-apt-repository -y ppa:git-core/ppa
+sudo add-apt-repository -y ppa:nginx/development
+sudo add-apt-repository -y ppa:chris-lea/redis-server
+wget http://dev.mysql.com/get/$mysql | sudo dpkg -i $mysql
+if [[ $node eq 4 ]]; then
+	curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+fi
+if [[ $node eq 5 ]]; then
+	curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+fi
+sudo apt-get upgrade
+sudo apt-get install -y build-essential mysql-server nodejs nginx
