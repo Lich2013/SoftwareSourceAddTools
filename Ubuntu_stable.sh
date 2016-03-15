a065098ea1563a98343514bc6af98cc9780dec71
Lich2013 <longzy2013@gmail.com>
2016-03-15 22:33:20 +0800

    debug

--- Ubuntu_stable.sh
+++ Ubuntu_stable.sh
@@ -2,15 +2,16 @@
 source='mirrors.aliyun.com' #镜像源
 mysql='mysql-apt-config_0.6.0-1_all.deb' #mysql源
 node=4 #选用4或5
-sudo sed -ig  "s@//[^/]*@//$source@" /etc/apt/source.list
+sudo sed -ig  "s@//[^/]*@//$source@" /etc/apt/sources.list
+sudo apt-get update
 sudo add-apt-repository -y ppa:git-core/ppa
 sudo add-apt-repository -y ppa:nginx/stable
 sudo add-apt-repository -y ppa:chris-lea/redis-server
 wget http://dev.mysql.com/get/$mysql | sudo dpkg -i $mysql
-if [[ $node eq 4 ]]; then
+if [[ $node -eq 4 ]]; then
 	curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 fi
-if [[ $node eq 5 ]]; then
+if [[ $node -eq 5 ]]; then
 	curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 fi
 sudo apt-get upgrade
